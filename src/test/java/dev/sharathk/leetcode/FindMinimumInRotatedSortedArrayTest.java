3706555e685c557f034167684392afb7242fc486
sharathk-dev <chandra.sharath2@gmail.com>
2026-09-13 01:07:30 +0530

    Add LeetCode 153 Find Minimum in Rotated Sorted Array stub and tests

---
+++ src/test/java/dev/sharathk/leetcode/FindMinimumInRotatedSortedArrayTest.java
@@ -0,0 +1,43 @@
+package dev.sharathk.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FindMinimumInRotatedSortedArrayTest {
+
+    @Test
+    void example1() {
+        assertEquals(1, FindMinimumInRotatedSortedArray.findMin(new int[]{3, 4, 5, 1, 2}));
+    }
+
+    @Test
+    void example2() {
+        assertEquals(0, FindMinimumInRotatedSortedArray.findMin(new int[]{4, 5, 6, 7, 0, 1, 2}));
+    }
+
+    @Test
+    void example3() {
+        assertEquals(11, FindMinimumInRotatedSortedArray.findMin(new int[]{11, 13, 15, 17}));
+    }
+
+    @Test
+    void singleElement() {
+        assertEquals(1, FindMinimumInRotatedSortedArray.findMin(new int[]{1}));
+    }
+
+    @Test
+    void twoElementsRotated() {
+        assertEquals(1, FindMinimumInRotatedSortedArray.findMin(new int[]{2, 1}));
+    }
+
+    @Test
+    void rotatedByOne() {
+        assertEquals(1, FindMinimumInRotatedSortedArray.findMin(new int[]{2, 3, 4, 5, 1}));
+    }
+
+    @Test
+    void minimumAtFrontMeansNoRotation() {
+        assertEquals(1, FindMinimumInRotatedSortedArray.findMin(new int[]{1, 2, 3, 4, 5}));
+    }
+}
---
+++ stubs/dev/sharathk/leetcode/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,19 @@
+package dev.sharathk.leetcode;
+
+public class FindMinimumInRotatedSortedArray {
+    /**
+     * LeetCode 153. Find Minimum in Rotated Sorted Array
+     *
+     * <p>Returns the minimum element of {@code nums}, an ascending array of unique
+     * values that has been rotated between {@code 1} and {@code nums.length} times.
+     *
+     * <p>Example: {@code findMin(new int[]{3, 4, 5, 1, 2})} returns {@code 1}.
+     *
+     * @param nums the rotated ascending array of unique integers
+     * @return the minimum element of {@code nums}
+     */
+    public static int findMin(int[] nums) {
+        // TODO: implement
+        throw new UnsupportedOperationException();
+    }
+}
